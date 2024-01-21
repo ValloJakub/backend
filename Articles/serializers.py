42187b165f126505d6b229adb462770c008a25c9
jakub <vallo@stud.uniza.sk>
2024-01-21 10:27:45 +0100

    Refactoring

--- Articles/serializers.py
+++ Articles/serializers.py
@@ -2,21 +2,22 @@ from rest_framework import serializers
 from .models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Article
         fields = ['id', 'specification', 'title', 'description', 'image', 'created_at']
 
-        def validate_title(self, value):
-            if len(value) < 20 or len(value) > 100:
-                raise serializers.ValidationError("Title must be between 20 and 100 characters.")
-            return value
+    def validate_title(self, value):
+        if len(value) < 20 or len(value) > 100:
+            raise serializers.ValidationError("Title must be between 20 and 100 characters.")
+        return value
 
-        def validate_specification(self, value):
-            if not value:
-                raise serializers.ValidationError("Specification must be selected.")
-            return value
+    def validate_specification(self, value):
+        if not value:
+            raise serializers.ValidationError("Specification must be selected.")
+        return value
 
-        def validate_description(self, value):
-            if not value.strip():
-                raise serializers.ValidationError("Description cannot be empty.")
-            return value
+    def validate_description(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Content cannot be empty.")
+        return value
